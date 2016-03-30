300a060bca3ee21447f4cd034a1dc8651fcf3416
berkin768 <berkinnakaydinn@gmail.com>
2016-03-30 06:27:55 +0300

    %90 finished

--- assigment1.c
+++ assigment1.c
@@ -1,5 +1,6 @@
 //gcc assigment1.c -o assigment1  -I/usr/include/libxml2 -lxml2 -ljson
 //./assigment1 xmlInput.xml
+//./assigment1 jsonInput.json
 
 #include "stdio.h"
 #include "libxml/parser.h"
@@ -15,8 +16,9 @@ xmlDocPtr jsonToXML_DOC = NULL;
 xmlNodePtr jsonToXML_root = NULL;
 json_object *JSONroot_object = NULL;
 
+//JSON -> XML BELOW
 
-void jsonParse(json_object *jobj, xmlNodePtr xmlRoot) {
+void createXML(json_object *jobj, xmlNodePtr xmlRoot) {
   json_object *firstJobj;
   xmlNodePtr newNode;
   enum json_type type;
@@ -38,20 +40,14 @@ void jsonParse(json_object *jobj, xmlNodePtr xmlRoot) {
 
       for (i = 0; i < arrayLength; i++) {
         arrayObjects = json_object_array_get_idx(jsonArray, i);
-        printf("%s\n", json_object_get_string(val));
-        json_object_object_foreach(arrayObjects, key, val){
-
-          printf("%s\n", key);
-          printf("%s\n", json_object_get_string(val));
-          xmlNewChild(newNode, NULL, BAD_CAST key, json_object_get_string(val));
-        }
+        xmlNewChild(newNode,NULL,BAD_CAST key, json_object_get_string(arrayObjects));
       }
       break;
 
       case json_type_object:
       firstJobj = json_object_object_get(jobj,key);
       newNode = xmlNewChild(xmlRoot, NULL, BAD_CAST key, NULL);
-      jsonParse(firstJobj,newNode);
+      createXML(firstJobj,newNode);
       break;
 
       default:
@@ -61,17 +57,30 @@ void jsonParse(json_object *jobj, xmlNodePtr xmlRoot) {
   }
 }
 
-void jsonToXML(char *parsedInput,json_object *json){
+void JSONToXML(char *parsedInput,json_object *json){
   jsonToXML_DOC = xmlNewDoc(BAD_CAST "1.0");
   jsonToXML_root = xmlNewNode(NULL, BAD_CAST"root");
   xmlDocSetRootElement(jsonToXML_DOC,jsonToXML_root);
 
-  jsonParse(json,jsonToXML_root);
+  createXML(json,jsonToXML_root);
 
   htmlSaveFileEnc(strcat(strcat(parsedInput,"Output"),".xml"), jsonToXML_DOC, "UTF-­8", 1);
 }
 
 //XML -> JSON BELOW
+
+void hasAttribute(xmlNode *currentNode,json_object *jsonRoot){
+  json_object *newObj;
+  xmlAttr *NodeAttribute = NULL;   //attribute as an object
+  xmlChar *attribute;
+  for (NodeAttribute = currentNode->properties; NodeAttribute; NodeAttribute = NodeAttribute->next) {
+    if (currentNode->type == XML_ELEMENT_NODE) {
+      attribute =  xmlNodeGetContent((xmlNode*)NodeAttribute);
+      newObj = json_object_new_string((char*)attribute);
+      json_object_object_add(jsonRoot,NodeAttribute->name,newObj);
+    }
+  }
+}
 
 int isChild(xmlNode *currentNode){
   for (currentNode = currentNode->children; currentNode; currentNode = currentNode->next) {
@@ -84,8 +93,6 @@ int isChild(xmlNode *currentNode){
 
 void xmlToJSON(xmlNode *a_node, json_object *jsonRoot){
   xmlNode *currentNode = NULL;     //node
-  xmlAttr *NodeAttribute = NULL;   //attribute as an object
-  xmlChar *attribute;              //char or char array
   json_object *newObj;
   json_object *newArray;
 
@@ -109,17 +116,22 @@ void xmlToJSON(xmlNode *a_node, json_object *jsonRoot){
           }
           same = 1;
           json_object_array_add(newArray,newObj);
+          if(currentNode->properties){
+            hasAttribute(currentNode,newObj);
+          }
           continue;
         }
 
         else{
           if(strcmp(lastElementName, currentNode->name) == 0){
-
             json_object_array_add(newArray,newObj);
           }
           else{
             json_object_object_add(jsonRoot,currentNode->name,newObj);
           }
+        }
+        if(currentNode->properties){
+          hasAttribute(currentNode,newObj);
         }
       }
 
@@ -127,10 +139,15 @@ void xmlToJSON(xmlNode *a_node, json_object *jsonRoot){
         same = 0;
         newObj = json_object_new_object();
         json_object_object_add(jsonRoot,currentNode->name,newObj);
+        if(currentNode->properties){
+          hasAttribute(currentNode,newObj);
+        }
         xmlToJSON(currentNode->children,newObj);
+
       }
     }
   }
+  free(lastElementName);
 }
 
 void readXML(char *xmlName, json_object *jsonRoot){
@@ -168,7 +185,7 @@ void operations(int argc, char *argv[]){
     jsonName = malloc(strlen(argv[1])+1);
     strcpy(jsonName, argv[1]);
     json_object *mainJSON_Object = json_object_from_file(jsonName);
-    jsonToXML(parsedInput[1],mainJSON_Object);
+    JSONToXML(parsedInput[1],mainJSON_Object);
   }
 
 }
